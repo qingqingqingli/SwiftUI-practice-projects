48c82e3a7778c91837cc730845c8f9a92fe16f61
Qing Li <qing.li8@ingka.ikea.com>
2022-07-25 17:28:21 +0200

    add sleep calculator model to project 4

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,24 +5,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours",
-                    value: $sleepAmount,
-                    in: 4...12,
-                    step: 0.5
-            )
-            // So screen readers can use VoiceOver
-            DatePicker("Please enter a date",
-                       selection: $wakeUp,
-                       in: Date.now...,
-                       displayedComponents: .hourAndMinute)
-                .labelsHidden()
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours",
+                        value: $sleepAmount,
+                        in: 4...12,
+                        step: 0.25
+                )
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+                        value: $coffeeAmount,
+                        in: 1...20
+                )
+            }
+            .padding()
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: calculatedBedTime)
+            }
         }
-        .padding()
+    }
+    
+    func calculatedBedTime() {
+        
     }
 }
 
