1b5db27a965dc1d6a21e1e643ee71aafa2ae1e2e
Qing Li <qing.li8@ingka.ikea.com>
2022-07-29 15:33:22 +0200

    updated project 4 based on challenges

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -25,45 +25,46 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    
+                Section(header: Text("When do you want to wake up?").font(.headline)) {
                     DatePicker("Please enter a time",
                                selection: $wakeUp,
                                displayedComponents: .hourAndMinute)
-                        .labelsHidden()
+                    .labelsHidden()
+                    .onChange(of: wakeUp) { _ in
+                        calculatedBedTime()
+                    }
                 }
-
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    
+                
+                Section(header: Text("Desired amount of sleep").font(.headline)) {
                     Stepper("\(sleepAmount.formatted()) hours",
                             value: $sleepAmount,
                             in: 4...12,
                             step: 0.25
                     )
+                    .onChange(of: sleepAmount) { _ in
+                        calculatedBedTime()
+                    }
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
-                            value: $coffeeAmount,
-                            in: 1...20
-                    )
+                Section(header: Text("Daily coffee intake").font(.headline)) {
+                    Picker(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+                           selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                           .onChange(of: coffeeAmount) { newValue in
+                               calculatedBedTime()
+                           }
+                }
+                
+                Section(header: Text("Your ideal sleep time is...").font(.headline)) {
+                    Text("\(alertMessage)")
                 }
             }
             .navigationTitle("Better Rest")
-            .toolbar {
-                Button("Calculate", action: calculatedBedTime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") {}
-            } message: {
-                Text(alertMessage)
+            .onAppear {
+                calculatedBedTime()
             }
         }
     }
@@ -87,8 +88,6 @@ struct ContentView: View {
             alertTitle = "Error"
             alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
-        
-        showingAlert = true
     }
 }
 
