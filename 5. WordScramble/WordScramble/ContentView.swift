bb68d9515d0412c8b476bd68d6e263fd19dc031d
Qing Li <qing.li8@ingka.ikea.com>
2022-08-01 10:49:46 +0200

    load start.txt in project 5

--- 5. WordScramble/WordScramble/ContentView.swift
+++ 5. WordScramble/WordScramble/ContentView.swift
@@ -5,12 +5,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     
     var body: some View {
-        List(people, id: \.self) {
-            Text($0)
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle.fill") // go up to 50
+                            Text(word)
+                            
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            // better user experience to insert the word at the beginning of the array
+            usedWords.insert(answer, at: 0)
+        }
+        print(usedWords)
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        // error handling
+        fatalError("Could not load start.txt")
     }
 }
 
