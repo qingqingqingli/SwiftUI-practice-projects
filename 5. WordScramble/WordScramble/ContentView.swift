ad609c54e09073f5c6a48326314688035c92445c
Qing Li <qing.li8@ingka.ikea.com>
2022-08-01 09:52:56 +0200

    update project naming

---
+++ 5. WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
+    var body: some View {
+        List(people, id: \.self) {
+            Text($0)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 5. WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Qing Li on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
