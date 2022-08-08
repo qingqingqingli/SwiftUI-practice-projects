bcd01f92bb417121e8cadc2d72efc4ec2334e7f1
Qing Li <qing.li8@ingka.ikea.com>
2022-08-08 09:20:23 +0200

    update project 6 animation

--- 6. Animations/Animations/ContentView.swift
+++ 6. Animations/Animations/ContentView.swift
@@ -5,9 +5,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.yellow)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        // no blur by default
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(
+            .easeInOut(duration: 1)
+            // be careful with the repeat count
+            // The program always needs to come back to match the state
+            .repeatCount(2, autoreverses: true),
+            value: animationAmount
+        )
+        
     }
 }
 
