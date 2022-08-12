5009371c6700de5de7d11477f19b6ea49748885f
Qing Li <qing.li8@ingka.ikea.com>
2022-08-12 13:40:00 +0200

    add drag gesture animation array

--- 6. Animations/Animations/ContentView.swift
+++ 6. Animations/Animations/ContentView.swift
@@ -5,30 +5,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
-
+    
+    @State private var isShowingColor = false
+    
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .yellow : .blue)
-                    .foregroundColor(.white)
-                    .offset(dragAmount)
-                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    isShowingColor.toggle()
+                }
+            }
+            
+            if isShowingColor {
+                Rectangle()
+                    .fill(.yellow)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
     }
 }
 
