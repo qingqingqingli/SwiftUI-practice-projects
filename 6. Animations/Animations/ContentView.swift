df7cf477f64b73f3937322f4feddb513dd83c456
Qing Li <qing.li8@ingka.ikea.com>
2022-08-12 13:31:35 +0200

    add drag gesture animation array

--- 6. Animations/Animations/ContentView.swift
+++ 6. Animations/Animations/ContentView.swift
@@ -5,23 +5,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
     @State private var dragAmount = CGSize.zero
-    
+
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount = $0.translation }
-                    .onEnded { _ in
-                        withAnimation {
-                            dragAmount = .zero
-                        }
-                    }
-            )
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .yellow : .blue)
+                    .foregroundColor(.white)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
