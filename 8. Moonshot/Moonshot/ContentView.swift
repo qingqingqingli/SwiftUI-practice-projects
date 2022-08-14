2955b5c3c8f17fda7917f3b26a1356bb0d8025c4
Qing Li <qing.li8@ingka.ikea.com>
2022-08-14 19:04:11 +0200

    add NavigationLink demo

--- 8. Moonshot/Moonshot/ContentView.swift
+++ 8. Moonshot/Moonshot/ContentView.swift
@@ -6,34 +6,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView(.vertical) {
-            // make it lazy stack so the view is only created when needed
-            LazyVStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item: \($0)")
-                        .font(.title)
+        NavigationView {
+            List {
+                ForEach(0..<99) { row in
+                    NavigationLink {
+                        Text("Detail \(row)")
+                    } label: {
+                        Text("Row \(row)")
+                    }
                 }
+                .navigationTitle("SwiftUI")
             }
-            // when it's a regular HStack or VStack
-            // the size of ScrollView is based on the children content
-            // so the maxWidth needs to be set on VStack not ScrollView
-            // however when it's a lazy stack
-            // it automatically takes up as much as space as possible
-//            .frame(maxWidth: .infinity)
+            .navigationViewStyle(.stack)
         }
-    }
-}
-
-struct CustomText: View {
-    let text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(_ text: String) {
-        print("Creating a new CustomerText \(text)")
-        self.text = text
     }
 }
 
