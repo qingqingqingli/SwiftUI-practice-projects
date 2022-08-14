b560ae08f94c9419d1478e72aa751ba247fcd097
Qing Li <qing.li8@ingka.ikea.com>
2022-08-14 19:10:20 +0200

    add example of Codable

--- 8. Moonshot/Moonshot/ContentView.swift
+++ 8. Moonshot/Moonshot/ContentView.swift
@@ -6,20 +6,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(0..<99) { row in
-                    NavigationLink {
-                        Text("Detail \(row)")
-                    } label: {
-                        Text("Row \(row)")
-                    }
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
-                .navigationTitle("SwiftUI")
             }
-            .navigationViewStyle(.stack)
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
         }
     }
+}
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
 }
 
 struct ContentView_Previews: PreviewProvider {
