aae4126520b3f80733479e2c79ec83a0585f48d6
Qing Li <qing.li8@ingka.ikea.com>
2022-08-19 20:55:45 +0200

    Add project 9 files

---
+++ 9. Drawing/Drawing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        // use GeometryReader to scale them relative to their container
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+        }
+        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 9. Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,14 @@
+//
+//  DrawingApp.swift
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
