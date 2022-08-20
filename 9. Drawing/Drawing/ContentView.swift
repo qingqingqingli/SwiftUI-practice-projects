c41f0bfbd020cf80738ba155a18424e25ffb1419
Qing Li <qing.li8@ingka.ikea.com>
2022-08-20 09:33:47 +0200

    add insettable shape protocol

--- 9. Drawing/Drawing/ContentView.swift
+++ 9. Drawing/Drawing/ContentView.swift
@@ -6,36 +6,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Arc(startAngle: .degrees(0),
-            endAngle: .degrees(180),
-            clockwise: true)
-        .stroke(.blue, lineWidth: 10)
-        .frame(width: 300, height: 300)
-        
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 40)
     }
 }
 
-struct Arc: Shape {
+// strokeBorder will shrink the shape
+// therefore, the shape should be insettable
+struct Arc: InsettableShape {
     var startAngle: Angle
     var endAngle: Angle
     var clockwise: Bool
+    var insetAmount = 0.0
 
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
         let modifiedStart = startAngle - rotationAdjustment
         let modifiedEnd = endAngle - rotationAdjustment
-        
+
         var path = Path()
-        path.addArc(
-            center: CGPoint(x: rect.midX, y: rect.midY),
-            radius: rect.width / 2,
-            startAngle: modifiedStart,
-            endAngle: modifiedEnd,
-            clockwise: clockwise
-        )
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
 
         return path
     }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
