a78702b7c9e52bf96f12e7695a73e448936e2a1e
Qing Li <qing.li8@ingka.ikea.com>
2022-09-07 20:51:12 +0200

    add initial files for project 10

---
+++ 10. CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Qing Li on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- 9. Drawing/Drawing/ContentView.swift
+++ 9. Drawing/Drawing/ContentView.swift
@@ -4,43 +4,64 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
+struct Flower: Shape {
+    // how much to move this petal away from the center
+    var petalOffset = -20.0
+    
+    // how wide to make each petal
+    var petalWidth = 100.0
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8 ) {
+            
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // move the petal to the center of the view
+            let movement = CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2)
+        
+            // combine the two transformation
+            let transform = rotation.concatenating(movement)
+                        
+            // create a path for original petal
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            // apply tranformation to the path
+            let transformedPetal = originalPetal.applying(transform)
+            
+            path.addPath(transformedPetal)
+        }
+        
+        return path
     }
 }
 
-// strokeBorder will shrink the shape
-// therefore, the shape should be insettable
-struct Arc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
-    var insetAmount = 0.0
-
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-
-        var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-
-        return path
+struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
+    var body: some View {
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(.red, style: FillStyle(eoFill: true))
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
     }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
-    
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
