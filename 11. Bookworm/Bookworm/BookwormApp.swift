1151bd04480e9fea4258733fde0659013b21c29b
Qing Li <qing.li8@ingka.ikea.com>
2022-09-11 15:55:29 +0200

    add initial files for project 11

---
+++ 11. Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,14 @@
+//
+//  BookwormApp.swift
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
