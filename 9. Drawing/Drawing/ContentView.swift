4c21dba17e5432bac90cfe2cfe4576ae61eade1c
Qing Li <qing.li8@ingka.ikea.com>
2022-12-26 16:22:20 +0100

    Add demo for animatableData

--- 9. Drawing/Drawing/ContentView.swift
+++ 9. Drawing/Drawing/ContentView.swift
@@ -4,38 +4,56 @@
 
 import SwiftUI
 
-struct Trapezoid: Shape {
-    var insetAmount: Double
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
     
-    /// Behind the scenes, SwiftUI set the animatableData property of our shape to the latest value
-    /// It's down to us on what to do with it
-    var animatableData: Double {
-        get { insetAmount }
-        set { insetAmount = newValue }
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+           AnimatablePair(Double(rows), Double(columns))
+        }
+
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
     }
 
     func path(in rect: CGRect) -> Path {
         var path = Path()
 
-        path.move(to: CGPoint(x: 0, y: rect.maxY))
-        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
 
         return path
-   }
+    }
 }
 
 struct ContentView: View {
-    @State private var insetAmount = 50.0
+    @State private var rows = 4
+    @State private var columns = 4
 
     var body: some View {
-        Trapezoid(insetAmount: insetAmount)
-            .frame(width: 200, height: 100)
+        Checkerboard(rows: rows, columns: columns)
             .onTapGesture {
-                withAnimation {
-                        insetAmount = Double.random(in: 10...90)
+                withAnimation(.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
                 }
             }
     }
