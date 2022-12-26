50455439b3d76160b68abfc0af8cfbe984fbb619
Qing Li <qing.li8@ingka.ikea.com>
2022-12-26 15:32:28 +0100

    Add demo for drawingGroup

--- 9. Drawing/Drawing/ContentView.swift
+++ 9. Drawing/Drawing/ContentView.swift
@@ -4,64 +4,52 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    // how much to move this petal away from the center
-    var petalOffset = -20.0
-    
-    // how wide to make each petal
-    var petalWidth = 100.0
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8 ) {
-            
-            // rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
-            
-            // move the petal to the center of the view
-            let movement = CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2)
-        
-            // combine the two transformation
-            let transform = rotation.concatenating(movement)
-                        
-            // create a path for original petal
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            
-            // apply tranformation to the path
-            let transformedPetal = originalPetal.applying(transform)
-            
-            path.addPath(transformedPetal)
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    let steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
         }
-        
-        return path
+        /// Tells SwiftUI to render the contents into an off-screen image before putting it back onto the screen
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
-    
+    @State private var colorCycle = 0.0
+
     var body: some View {
         VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-        }
-    }
-}
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
+            Slider(value: $colorCycle)
+        }
     }
 }
